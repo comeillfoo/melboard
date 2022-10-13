897b2fe1ffad54a604e56c0c1a922f4b1a2213aa
Come1LLF00 <rayveleaveme34@inbox.ru>
2022-10-14 01:35:05 +0300

    feat: more settings for timers and usart

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,8 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "tim.h"
+#include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -84,6 +86,8 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_TIM1_Init();
+  MX_USART6_UART_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
