867889e5ed8462690b08361441f8815a6ab9416d
Come1LLF00 <rayveleaveme34@inbox.ru>
2022-10-25 14:55:24 +0300

    feat: more perfect frequencies

--- Core/Inc/buzzer.h
+++ Core/Inc/buzzer.h
@@ -19,6 +19,65 @@ struct buzzer {
 };
 
 
+#define N_C0  16352
+#define N_D0  18354
+#define N_E0  20602
+#define N_F0  21827
+#define N_G0  24500
+#define N_A0  27500
+#define N_B0  30868
+#define N_C1  32703
+#define N_D1  36708
+#define N_E1  41203
+#define N_F1  43654
+#define N_G1  48999
+#define N_A1  55000
+#define N_B1  61735
+#define N_C2  65406
+#define N_D2  73416
+#define N_E2  82407
+#define N_F2  87307
+#define N_G2  97999
+#define N_A2  110000
+#define N_B2  123471
+#define N_C3  130813
+#define N_D3  146832
+#define N_E3  164814
+#define N_F3  176614
+#define N_G3  195998
+#define N_A3  220000
+#define N_B3  246942
+#define N_C4  261626
+#define N_D4  293665
+#define N_E4  329628
+#define N_F4  349228
+#define N_G4  391995
+#define N_A4  440000
+#define N_B4  493883
+//#define N_C5  523
+//#define N_D5  587
+//#define N_E5  659
+//#define N_F5  698
+//#define N_G5  784
+//#define N_A5  880
+//#define N_B5  988
+//#define N_C6  1047
+//#define N_D6  1175
+//#define N_E6  1319
+//#define N_F6  1397
+//#define N_G6  1568
+//#define N_A6  1760
+//#define N_B6  1976
+//#define N_C7  2093
+//#define N_D7  2349
+//#define N_E7  2637
+//#define N_F7  2794
+//#define N_G7  3136
+//#define N_A7  3520
+//#define N_B7  3951
+//#define N_C8  4186
+//#define N_D8  4699
+
 void raise_octave(struct fifo_queue*, enum request_type);
 
 void lower_octave(struct fifo_queue*, enum request_type);
--- Core/Src/buzzer.c
+++ Core/Src/buzzer.c
@@ -15,31 +15,69 @@
 
 static struct buzzer buzz = { DEFAULT_DURATION, DEFAULT_OCTAVE, 1 };
 
-enum note_frequency { // in milliherz for first octave (5)
-//	NF_DO  = 261630,
-//	NF_RE  = 293670,
-//	NF_MI  = 329630,
-//	NF_FA  = 349230,
-//	NF_SOL = 392000,
-//	NF_LA  = 440000,
-//	NF_TI  = 493880
-	NF_DO  = 16352,
-	NF_RE  = 18354,
-	NF_MI  = 20602,
-	NF_FA  = 21827,
-	NF_SOL = 24500,
-	NF_LA  = 27500,
-	NF_TI  = 30868
-};
+//enum note_frequency { // in milliherz for first octave (5)
+////	NF_DO  = 261630,
+////	NF_RE  = 293670,
+////	NF_MI  = 329630,
+////	NF_FA  = 349230,
+////	NF_SOL = 392000,
+////	NF_LA  = 440000,
+////	NF_TI  = 493880
+//	NF_DO  = 16351,
+//	NF_RE  = 18354,
+//	NF_MI  = 20602,
+//	NF_FA  = 21827,
+//	NF_SOL = 24500,
+//	NF_LA  = 27500,
+//	NF_TI  = 30868
+//};
 
-static enum note_frequency millifreq_map[] = {
-		[RQT_DO]  = NF_DO,
-		[RQT_RE]  = NF_RE,
-		[RQT_MI]  = NF_MI,
-		[RQT_FA]  = NF_FA,
-		[RQT_SOL] = NF_SOL,
-		[RQT_LA]  = NF_LA,
-		[RQT_TI]  = NF_TI
+//static enum note_frequency millifreq_map[] = {
+//		[RQT_DO]  = NF_DO,
+//		[RQT_RE]  = NF_RE,
+//		[RQT_MI]  = NF_MI,
+//		[RQT_FA]  = NF_FA,
+//		[RQT_SOL] = NF_SOL,
+//		[RQT_LA]  = NF_LA,
+//		[RQT_TI]  = NF_TI
+//};
+
+static uint32_t herz_map[] = {
+	N_C0,
+	N_D0,
+	N_E0,
+	N_F0,
+	N_G0,
+	N_A0,
+	N_B0,
+	N_C1,
+	N_D1,
+	N_E1,
+	N_F1,
+	N_G1,
+	N_A1,
+	N_B1,
+	N_C2,
+	N_D2,
+	N_E2,
+	N_F2,
+	N_G2,
+	N_A2,
+	N_B2,
+	N_C3,
+	N_D3,
+	N_E3,
+	N_F3,
+	N_G3,
+	N_A3,
+	N_B3,
+	N_C4,
+	N_D4,
+	N_E4,
+	N_F4,
+	N_G4,
+	N_A4,
+	N_B4
 };
 
 
@@ -79,7 +117,7 @@ void unmute_buzzer() { TIM1->CCR1 = TIM1->ARR >> 1; }
 
 
 static void set_frequency(uint32_t millifreq) {
-	TIM1->PSC = ((2 * HAL_RCC_GetPCLK2Freq() * 10) / (2 * (TIM1->ARR) * millifreq)) * 100 - 1;
+	TIM1->PSC = ((2 * HAL_RCC_GetPCLK2Freq()) / (2 * (TIM1->ARR) * (millifreq / 1000))) - 1;
 }
 
 
@@ -94,14 +132,14 @@ static char notes[] = {
 };
 
 void play_note(struct fifo_queue* response_q, enum request_type req) {
-	const uint32_t millifreq_of_octave = millifreq_map[req] << (buzz.octave - 1);
-	set_frequency(millifreq_of_octave);
+	const uint32_t freq_of_octave = herz_map[req + 7 * (buzz.octave - 1)];
+	set_frequency(freq_of_octave);
 	unmute_buzzer();
 	buzz.done = 0;
 
 	// send response
 	char response[DEFAULT_RESPONSE_LENGTH];
-	snprintf(response, DEFAULT_RESPONSE_LENGTH, "%c%u %lu,%lu\r\n", notes[req], buzz.octave, buzz.duration / 10, buzz.duration % 10);
+	snprintf(response, DEFAULT_RESPONSE_LENGTH, "%c%u %lu,%lu\r\n", notes[req], buzz.octave - 1, buzz.duration / 10, buzz.duration % 10);
 	const size_t length = strlen(response);
 	queue_write(response_q, (uint8_t*) response, length);
 }
